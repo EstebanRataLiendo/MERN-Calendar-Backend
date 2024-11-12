b4f024b08ff74a553fc2fae50df70dac3ab4c8d0
EstebanRataLiendo <esteban.cba92@gmail.com>
2024-11-12 20:17:48 -0300

    App Desplegada

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
@@ -21,6 +23,10 @@ app.use( express.json());
 //rutas
 app.use( '/api/auth', require('./routes/auth') )
 app.use( '/api/events', require('./routes/events') )
+
+app.use('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 
 //excuchar peticiones
 app.listen(process.env.PORT, () => {
